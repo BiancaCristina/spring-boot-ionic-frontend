79442ddb295d9c5a5436f98626d02b4f974840ce
BiancaCristina <phon-bia@hotmail.com>
2018-08-15 01:15:40 -0300

    Botao de logout no menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AuthService } from '../services/auth.service';
 
 // Esse arquivo eh o controlador da pagina "app.html"
 @Component({
@@ -16,16 +17,20 @@ export class MyApp {
   // O comando abaixo define uma colecao de objetos com os elementos titulo e componente
   pages: Array<{title: string, component: string}>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  constructor(
+    public platform: Platform, 
+    public statusBar: StatusBar, 
+    public splashScreen: SplashScreen,
+    public authService: AuthService) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
     // O comando abaixo define uma lista 
     this.pages = [ 
       { title: 'Profile', component: 'ProfilePage' },
-      { title: 'Categorias', component: 'CategoriasPage' }
+      { title: 'Categorias', component: 'CategoriasPage'},
+      { title: 'Logout', component: ''} // O Logout possui um tratamento especial, uma vez que nao possu component
     ];
-
   }
 
   initializeApp() {
@@ -37,9 +42,17 @@ export class MyApp {
     });
   }
 
-  openPage(page) {
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
+  openPage(page: {title: string, component: string}) {
+    // Esse metodo eh responsavel por fazer o tratamento especial do botao "logout"
+    switch(page.title){
+      case 'Logout': 
+        this.authService.logout(); // Chama o metodo logout do AuthService
+        this.nav.setRoot("HomePage"); // Redireciona pra pagina inicial
+      break;
+
+      default:
+        this.nav.setRoot(page.component);
+    }
+    
   }
 }
