6eeb776442913edd5267c06e9d8651cb642ba719
BiancaCristina <phon-bia@hotmail.com>
2018-08-25 21:33:46 -0300

    Testando o app no dispositivo

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,5 +1,5 @@
 // Define configuracoes da minha API
 export const API_CONFIG = {
-    baseURL: "http://localhost:8080",
+    baseURL: "https://spring-boot-ionic-bianca.herokuapp.com",
     bucketBaseURL: "https://s3-sa-east-1.amazonaws.com/spring-boot-ionic-bianca"
 }
